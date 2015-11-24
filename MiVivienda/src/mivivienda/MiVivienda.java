ff7ba8476d6d9d353e6e405806128e3cb503b4a9
fercalbla <fer@localhost.localdomain>
2015-11-24 20:37:33 +0100

    Ejercicios actualizados

--- MiVivienda/src/mivivienda/MiVivienda.java
+++ MiVivienda/src/mivivienda/MiVivienda.java
@@ -16,24 +16,85 @@ public class MiVivienda {
      */
     public static void main(String[] args) {
         Vivienda casa = new Vivienda();
+        Vivienda casa2 = new Vivienda();
+        Vivienda casa3 = new Vivienda();
         //set -> 3 inq, puertas true, ventanas true, alarma false, 70m, B, 1º, bloque 7, 5 habis, calf G
         //cambiar -> puertas a abierta, intentar temp a 30 grados, extraer valor temp, calf A, 2 habitantes mas
-            casa.inquilinos = 3;
-            casa.puertas = true;
-            casa.ventanas = true;
-            casa.alarma = false;
-            casa.asignar("metros", "70");
-            casa.asignar("piso", "8");
-            casa.asignar("bloque", "7");
+            System.out.println("Vivienda 1");
+            //ASIGNAR VALORES POR DEFECTO
+            casa.misInquilinos(3);
+            casa.misPuertas(true);
+            casa.misVentanas(true);
+            casa.miAlarma(false);
+            casa.misMetros(90);
+            casa.miLetra('C');
+            casa.miPiso(1);
+            casa.miBloque(7);
+            casa.misHabitaciones(5);
+            casa.miCalificacion('G');
+            casa.showInfo(false);
             
-        Vivienda casa2 = new Vivienda();
+            System.out.println("\n");
+            //CAMBIANDO VALORES
+            casa.misPuertas(false);
+            casa.miTemperatura(30);
+            casa.miCalificacion('A');
+            casa.misInquilinos(5);
+            casa.showInfo(true);
+            
+            System.out.println("\n\n");
+            
+        
         //5, puertas true, ventanas true, alarma false, 90m, B, 1º, bloque 7, 4 habis, calf B
         //cambiar -> 2 habitantes menos que la uno, cambia la calificacion al mismo valor de la 1 y la temperatura de la vivienda 3
+            System.out.println("\nVivienda 2");
+            //ASIGNAR VALORES POR DEFECTO
+            casa2.misInquilinos(5);
+            casa2.misPuertas(true);
+            casa2.misVentanas(true);
+            casa2.miAlarma(false);
+            casa2.misMetros(90);
+            casa2.miLetra('B');
+            casa2.miPiso(1);
+            casa2.miBloque(7);
+            casa2.misHabitaciones(4);
+            casa2.miCalificacion('B');
+            casa2.showInfo(false);
+            
+            System.out.println("\n");
+            //CAMBIANDO VALORES
+            casa2.miTemperatura(casa3.temperatura);
+            casa2.miCalificacion(casa.calificacion);
+            casa2.misInquilinos(casa.inquilinos - 2);
+            casa2.showInfo(true);
         
-        Vivienda casa3 = new Vivienda();
+        
         //1 inq, puertas false, ventanas false, alarma true, 70m, A, 1º, Bloque 7, 5 habis, calf G, desocupada
         //cambiar -> vivienda true, ventanas true, alarma false, habitantes 0, estado = derribo, temp = 18
-        
+            System.out.println("\nVivienda 3");
+            //VARIABLES POR DEFECTO
+            casa3.misInquilinos(1);
+            casa3.misPuertas(false);
+            casa3.misVentanas(false);
+            casa3.miAlarma(true);
+            casa3.misMetros(70);
+            casa3.miLetra('A');
+            casa3.miPiso(1);
+            casa3.miBloque(7);
+            casa3.misHabitaciones(5);
+            casa3.miCalificacion('G');
+            casa3.miSituacion("Desocupada");
+            casa3.showInfo(false);
+            
+            System.out.println("\n");
+            //VARIABLES CAMBIADAS
+            casa3.misPuertas(true);
+            casa3.misVentanas(true);
+            casa3.miAlarma(false);
+            casa3.misInquilinos(0);
+            casa3.miSituacion("Demolicion");
+            casa3.miTemperatura(18);
+            casa3.showInfo(true);
         
     }
     
--- MiVivienda/src/mivivienda/Vivienda.java
+++ MiVivienda/src/mivivienda/Vivienda.java
@@ -10,13 +10,13 @@ package mivivienda;
  * @author fer
  */
 public class Vivienda {
-    private int metros = 50;
-    private char letra = 'A';
-    private int piso = 1;
-    private int bloque = 1;
-    String calificacion = "G";
+    int metros = 50;
+    char letra = 'A';
+    int piso = 1;
+    int bloque = 1;
+    char calificacion = 'G';
     boolean alarma = false;
-    private int habitaciones = 3;
+    int habitaciones = 3;
     boolean ventanas = true;
     boolean puertas = true;
     int inquilinos = 0;
@@ -24,21 +24,164 @@ public class Vivienda {
     String situacion = "Desocupada";
     
     
-    
-    public void asignar(String valor,String nuevoValor){
-        valor = valor.toLowerCase();
-        if(valor == "metros"){
-            metros = Integer.parseInt(nuevoValor);
-        }else if(valor == "letras"){
-            letra = nuevoValor.charAt(0);
-        }else if (valor == "piso") {
-            piso = Integer.parseInt(nuevoValor);
-        }else if (valor == "bloque") {
-            bloque = Integer.parseInt(nuevoValor);
-        }else if (valor == "habitaciones") {
-            habitaciones = Integer.parseInt(nuevoValor);
+    public void misMetros(int metros){
+        if(metros >= 50){
+            this.metros = metros;
         }else{
-            System.out.println("El valor dado es erroneo.");
-        }    
+            System.out.println("\033[31mERROR: Argumento invalido. Ha de ser mayor o igual 50. | Resultado: " + (metros));
+        }
     }
+    
+    public void miLetra(char letra){
+        this.letra = letra;
+    }
+    
+    public void miPiso(int piso){
+        this.piso = piso;
+    }
+    
+    public void miBloque(int bloque){
+        this.bloque = bloque;
+    }
+    
+    public void miCalificacion(char calificacion){
+        switch(calificacion){
+            case 'A':
+                this.calificacion = calificacion;
+                break;
+            case 'B':
+                this.calificacion = calificacion;
+                break;
+            case 'C':
+                this.calificacion = calificacion;
+                break;
+            case 'D':
+                this.calificacion = calificacion;
+                break;
+            case 'E':
+                this.calificacion = calificacion;
+                break;
+            case 'F':
+                this.calificacion = calificacion;
+                break;
+            default:
+                this.calificacion = 'G';
+                break;
+            
+        }
+    }
+    
+    public void miAlarma(boolean alarma){
+        this.alarma = alarma;
+    }
+    
+    public void misHabitaciones(int habitaciones){
+        if(habitaciones < 3){
+            System.out.println("\033[31mERROR: Argumento invalido. Ha de ser mayor o igual a 3. | Resultado: " + (habitaciones) +"\n\033[31mSe ha asignado el valor por defecto.");
+        }else{
+            this.habitaciones = habitaciones;
+        }
+    }
+    
+    public void misVentanas(boolean ventanas){
+        this.ventanas = ventanas;
+    }
+    
+    public void misPuertas(boolean puertas){
+        this.puertas = puertas;
+    }
+    
+    public void misInquilinos(int inquilinos){
+        if(inquilinos < 0){
+            System.out.println("\033[31mERROR: Parametro invalido. Ha de ser mayor o igual a 0. | Resultado: " + (inquilinos) +"\n\033[31mSe ha asignado el valor por defecto.");
+        }else{
+            this.inquilinos = inquilinos;
+            situacion = "Ocupada";
+        }
+    }
+    
+    public void miTemperatura(int temperatura){
+        if(temperatura < 18 || temperatura > 26){
+            System.out.println("\033[31mERROR: Parametro invalido. Ha de ser mayor o igual a 18 O menor o igual a 26. | Resultado: " + (temperatura) +"\n\033[31mSe ha asignado el valor por defecto.");
+        }else{
+            this.temperatura = temperatura;
+        }
+    }
+    
+    public void miSituacion(String situacion){
+        situacion = situacion.toLowerCase();
+        switch(situacion){
+            case "ocupada":
+                if(inquilinos >= 0){
+                    System.out.println("\033[31mERROR: No se puede asignar esta situacion no si hay al menos un inquilino.");
+                }else{
+                    this.situacion = "Ocupada";
+                }
+                break;
+            case "desocupada":
+                if(inquilinos > 0){
+                    System.out.println("\033[31mERROR: No se puede asignar esta situacion si hay un inquilino.");
+                }else{
+                    this.situacion = "Desocupada";
+                }
+                break;
+            case "alquiler":
+                if(inquilinos > 0){
+                    System.out.println("\033[31mERROR: No se puede asignar esta situacion si hay un inquilino.");
+                }else{
+                    this.situacion = "Alquiler";
+                }
+                break;
+            case "venta":
+                if(inquilinos > 0){
+                    System.out.println("\033[31mERROR: No se puede asignar esta situacion si hay un inquilino.");
+                }else{
+                    this.situacion = "Venta";
+                }
+                break;
+            case "demolicion":
+                if(inquilinos > 0){
+                    System.out.println("\033[31mERROR: No se puede asignar esta situacion si hay un inquilino.");
+                }else{
+                    this.situacion = "Demolicion";
+                }
+                break;
+            default:
+                System.out.println("\033[31mERROR: Has introducido un parametro invalido, elige entre: ocupada, desocupada, alquiler, venta o demolicion.");
+        }
+    }
+    
+    public void showInfo(boolean isChanged){
+        if(isChanged == true){
+            System.out.println("La informacion de la vivienda ha sido cambiada. Estos son los nuevos parametros:");
+        }else{
+            System.out.println("La vivienda contiene los parametros por defecto, estos son:");
+        }
+        System.out.println("Metros de la vivienda: " + metros);
+        System.out.println("Letra de la vivienda: " + letra);
+        System.out.println("Piso de la vivienda: " + piso);
+        System.out.println("Bloque de la vivienda: " + bloque);
+        System.out.println("Calificacion de la vivienda: " + calificacion);
+        System.out.println("Habitaciones en la vivienda: " + habitaciones);
+        System.out.println("Inquilinos en la vivienda: " + inquilinos);
+        System.out.println("Situacion de la vivienda: " + situacion);
+        System.out.println("Temperatura de la vivienda: " + temperatura + "ºC");
+        
+        String alarmOn = (alarma == true) ? "Sí, está activada.":"No, no está activada";
+        System.out.println("¿La alarma esta activada? " + alarmOn);
+        String ventanasOpen = (ventanas == true) ? "Sí, las ventanas están abiertas.":"No, las ventanas están cerradas";
+        System.out.println("¿Están las ventanas abiertas? " + ventanasOpen);
+        String puertasOpen = (puertas == true) ? "Sí, las puertas están abiertas.":"No, las puertas están cerradas";
+        System.out.println("¿Están las puertas abiertas? " + puertasOpen);
+    }
+    
+    /*public String getInfo(String value){
+        String toReturn = "";
+        value = value.toLowerCase();
+        switch(value){
+            case "metros":
+                toReturn = metros.toString();
+            
+        }
+    }*/
 }
